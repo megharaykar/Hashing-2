d47b1cdb3a91fce420e7b6f55d75e3c13d451d61
Megha Raykar <meghamanjunathraykar@Meghas-MacBook-Air.local>
2026-02-26 12:47:54 -0800

    Problem2 added code changes with just 1 pass

--- Problem2.py
+++ Problem2.py
@@ -1,12 +1,11 @@
 # https://leetcode.com/problems/contiguous-array/description/
 
 # Time Complexity: O(n), first pass is to create rsum_list and second pass is on the rsum_list to create a hashmap 
-# Space Complexity: O(n), rsum_list is of length n and then a constant hashmap space
+# Space Complexity: O(1), a constant hashmap space
 
-# This longest contiguous subarray problem implements the running sum. First we need to build a running sum array.
-# Running sum is calculated by this logic - Increment the rsum if it is 1, else decrement if it's 0. These rsum values 
-# are added into the new rsum_list. Then a dictionary is created with every first occurence of the rsum. Everytime we encounter
-# a rsum which is already existing in dictionary, that means we have a valid contiguous subarray with equal 0's and 1's(balanced). 
+# This longest contiguous subarray problem implements the running sum. 
+# Running sum is calculated by this logic - Increment the rsum if it is 1, else decrement if it's 0. A dictionary is created with every first occurence of the rsum. 
+# Everytime we encounter a rsum which is already existing in dictionary, that means we have a valid contiguous subarray with equal 0's and 1's(balanced). 
 # Balanced = When the difference between the curr rsum and previous rsum is 0(that means same rsum), then it's called balanced. 
 # The length of balanced subarray is calculated using indexing and max is updated in every iteration until we get the longest. 
 # We return the longest subarray length(which is also contiguous). The reason behind implementing with rsum is because we want to 
@@ -14,34 +13,26 @@
 # 
 # Example  [0, 1, 0,1,1,1,1,0,0,1,0,1,0,1,1,0,0,1,1] 
 #           0  1  2 3 4 5 6 7 8 9 ................18
-# rsum_list=[-1,0,-1,0,1,2,3,2,1,2,1,2,1,2,3,2,1,2,3]
+# rsum =   -1, 0,-1,0,1,2,3,2,1,2,1,2,1,2,3,2,1,2,3
 # hm => rsum : idx = {-1 : 0, 0 : 1, 1 : 4, 2 : 5, 3 : 6}
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         rsum = 0
-        rsum_list = []
         hm = {}
         result = 0
 
         hm[0] = -1
-        for num in nums:
+        for i, num in enumerate(nums):
             if num is 0:
                 rsum = rsum - 1
-                rsum_list.append(rsum)
             else:
                 rsum = rsum + 1
-                rsum_list.append(rsum)
 
-        print(rsum_list)
-
-        for i in range(len(rsum_list)):
-            if rsum_list[i] not in hm:
-                hm[rsum_list[i]] = i
+            if rsum not in hm:
+                hm[rsum] = i
             else:
-                result = max(result, i - hm[rsum_list[i]])
-
-        print(hm)
+                result = max(result, i - hm[rsum])
                    
         return result 
     
